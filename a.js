bbb06501e6f952efe8f1f31e0c68612b31db999d
Lam Thanh Hoai <lamthanhhoai1990@gmail.com>
2018-04-06 12:13:05 +0700

    Array them xoa co ban

---
+++ a.js
@@ -0,0 +1,14 @@
+// Khai bao
+//const arr = new Array();
+const arr = [1, 3, 2, 5, 7, 6, 8];
+
+//arr.pop();
+// arr.push(100);
+// arr.shift();
+// arr.unshift(0);
+//arr.sort();
+//arr.splice(2, 3);  //Remove 3 phan tu bat dau tu phan tu thu 2
+//arr.splice(2, 3, 100, 200); Remove 3 phan tu bat dau tu phan tu thu 2 va thay cac gia tri phia sau vao
+//arr.splice(2, 0, 100, 200); // Cach de them phan tu
+console.log(arr);
+console.log(arr[2])
