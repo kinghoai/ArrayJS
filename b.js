c42a25ceb7319a6b9639fcfad7f62a2417603eae
Lam Thanh Hoai <lamthanhhoai1990@gmail.com>
2018-04-06 12:27:56 +0700

    tham chieu va tham tri

---
+++ b.js
@@ -0,0 +1,13 @@
+const a = {
+    name : 'Teo',
+}
+const b = {
+    name : 'Teo',
+}
+console.log(a === b); //false
+
+const c = '5';
+const d = 5;
+const e = 5;
+console.log(c === d);// false
+console.log(d === e) //true
